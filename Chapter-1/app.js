9658f0083587c3db168b5946b1c72eba9564b946
Arsalan23c <153868088+Arsalan23c@users.noreply.github.com>
2024-02-24 02:52:32 +0500

    assignments-push

---
+++ Chapter-1/app.js
@@ -0,0 +1,35 @@
+ 
+  
+ 
+ var greeting = "Hello everyone" //string
+  
+    
+  
+alert( greeting
+
+)
+
+
+var error = "Error ! please enter a Valid Password" //string
+
+alert ( error)
+
+
+
+var welCome = " Welcome to JS Land... \n Happy Coding..."
+
+alert ( welCome)
+
+
+var welcome = " Welcome to JS Land..."
+
+alert ( welcome)
+
+
+var prevent = " Happy Coding... \n ▢ Prevent this page from creating additional dialogs"
+
+alert ( prevent)
+
+
+
+
---
+++ Chapter-3/app.js
@@ -0,0 +1,14 @@
+ 
+ 
+ var age = "17"
+
+ alert (age)
+
+ var Nvisit = "You have visit this site 15 times"
+
+ alert (Nvisit) 
+
+var birthyear ="2006"
+
+// alert ( birthyear)
+
